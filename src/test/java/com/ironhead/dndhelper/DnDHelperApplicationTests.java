eaf84251db8a39b31ddebf5d83e5d7dfb6900f57
CabageMan <1iron.head1@gmail.com>
2023-11-08 18:13:06 +0300

    Initial commit

---
+++ src/test/java/com/ironhead/dndhelper/DnDHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhead.dndhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDHelperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
