286dc0053dc8fc7db8190368964fc230da2f332c
CabageMan <1iron.head1@gmail.com>
2023-11-10 11:04:19 +0300

    Run Hello World with test

--- src/main/java/com/ironhead/dndhelper/DnDHelperApplication.java
+++ src/main/java/com/ironhead/dndhelper/DnDHelperApplication.java
@@ -2,10 +2,12 @@ package com.ironhead.dndhelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// Read more about annotation and solution.
+// https://stackoverflow.com/questions/51221777/failed-to-configure-a-datasource-url-attribute-is-not-specified-and-no-embedd
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class DnDHelperApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(DnDHelperApplication.class, args);
   }
